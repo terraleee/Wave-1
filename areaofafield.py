b7421f1282af43a6d51ad7d1750879dcc832c30b
terraleee <terra.lee22@ycdsbk12.ca>
2020-04-13 13:09:15 -0400

    adding 1.1 areaofafield

---
+++ areaofafield.py
@@ -0,0 +1,6 @@
+# formula = length*width/squarefeet
+
+length = float(input("Enter the length of the farmer's field in feet: "))
+width = float(input("Enter the width of the farmer's field in feet: "))
+area = length * width / 43560
+print("The area of the farmer's field is", area, "acres.")
